9b9c39eabe7ab949e5bc574cc16c024569f4f07e
divyajainsurana <43678200+divyajainsurana@users.noreply.github.com>
2020-05-19 00:57:13 +0800

    Update permutation_in_string.py

--- permutation_in_string.py
+++ permutation_in_string.py
@@ -1,3 +1,8 @@
+"""
+Given two strings s1 and s2, write a function to return true if s2 contains the permutation of s1. In other words, one of the first string's permutations is the substring of the second string.
+
+"""
+
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
         abc='abcdefghijklmnopqrstuvwxyz'
